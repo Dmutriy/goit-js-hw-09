78827f4e4d0276489d564ef4b05b8d52b5d32934
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2022-10-07 14:09:21 +0300

    03-promises

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,9 +2,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 require('flatpickr/dist/themes/dark.css');
-// require('flatpickr/dist/themes/dark.css');
 
-// Обираэмо кверіСелектори
+// Обираємо кверіСелектори
 const refs = {
   input: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('[data-start]'),
@@ -67,19 +66,14 @@ refs.startBtn.addEventListener('click', () => {
 });
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
@@ -87,88 +81,3 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
-
-// -_-_-_-_-_-_-_-_-_-_-_-=-=-=-=-=-=-=-=-=-=-=-=-
-
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// const TIMER_DEADLINE = new Date(2022, 8, 26, 21, 35);
-
-// const timerRef = document.querySelector('.timer__items');
-
-// const timer = {
-//   intervalId: null,
-//   refs: {},
-//   notifyOptions: {
-//     position: 'center-center',
-//     backOverlay: true,
-//     clickToClose: true,
-//     closeButton: true,
-//   },
-//   start(rootSelector, deadline) {
-//     const delta = deadline.getTime() - Date.now();
-//     if (delta <= 0) {
-//       Notify.failure(
-//         'Вибраний час в минулому, виберіть дату в майбутньому!',
-//         this.notifyOptions
-//       );
-//       return;
-//     }
-//     Notify.success('Відлік почався', this.notifyOptions);
-//     this.getRefs(rootSelector);
-//     this.intervalId = setInterval(() => {
-//       const diff = deadline.getTime() - Date.now();
-
-//       if (diff <= 1000) {
-//         clearInterval(this.intervalId);
-//         Notify.success('Дедлайн настав!', this.notifyOptions);
-//       }
-
-//       const data = this.convertMs(diff);
-//       //   Object.entries(data).forEach(([name, value]) => {
-//       //     this.refs[name].textContent = this.addLeadinZero(value);
-//       //   });
-//       this.refs.days.textContent = this.addLeadinZero(data.days);
-//       this.refs.hours.textContent = this.addLeadinZero(data.hours);
-//       this.refs.minutes.textContent = this.addLeadinZero(data.minutes);
-//       this.refs.seconds.textContent = this.addLeadinZero(data.seconds);
-//     }, 1000);
-//   },
-
-//   getRefs(rootSelector) {
-//     // [...rootSelector.children].forEach(item => {
-//     //   const { title } = item.dataset;
-//     //   this.refs[title] = item;
-//     // });
-//     this.refs.days = rootSelector.querySelector('.js-timer__days');
-//     this.refs.hours = rootSelector.querySelector('.js-timer__hours');
-//     this.refs.minutes = rootSelector.querySelector('.js-timer__minutes');
-//     this.refs.seconds = rootSelector.querySelector('.js-timer__seconds');
-//   },
-//   convertMs(diff) {
-//     const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-//     const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
-//     const minutes = Math.floor(diff / 1000 / 60) % 60;
-//     const seconds = Math.floor(diff / 1000) % 60;
-//     return { days, hours, minutes, seconds };
-//   },
-//   addLeadinZero(value) {
-//     return String(value).padStart(2, '0');
-//   },
-// };
-
-// // timer.start(timerRef, TIMER_DEADLINE);
-// console.log(new Date('2030-03-16'));
-// console.log(new Date('2030-03'));
-// console.log(new Date('2018'));
-
-// const interval1 = setInterval(function () {
-//   // body
-// }, 1000);
-
-// const interval2 = setInterval(function () {
-//   // body
-// }, 1000);
-
-// const interval3 = setInterval(function () {
-//   // body
-// }, 1000);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  btnSubmit: document.querySelector('.form'),
+};
+
+function createPromise(i, delayValue) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ i, delayValue });
+      } else {
+        reject({ i, delayValue });
+      }
+    }, delayValue);
+  });
 }
+
+refs.btnSubmit.addEventListener('submit', evt => {
+  evt.preventDefault();
+  let delayValue = Number(refs.delay.value);
+  let stepValue = Number(refs.step.value);
+  let amountValue = Number(refs.amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, delayValue)
+      .then(({ i, delayValue }) => {
+        return Notify.success(`Fulfilled promise ${i} in ${delayValue}ms`);
+      })
+      .catch(({ i, delayValue }) => {
+        return Notify.failure(`Rejected promise ${i} in ${delayValue}ms`);
+      });
+    delayValue += stepValue;
+  }
+});
